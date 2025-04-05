a4410f0777da18eecad6d1325d2836d59f88fa86
Jeffrey Lyon <jeffrey.lyon@ahead.com>
2025-04-05 12:34:39 -0400

    added netbox

---
+++ apps/netbox/netbox/overlays/dev/configuration/plugins.py
@@ -0,0 +1,8 @@
+PLUGINS = ['netbox_bgp','netbox_secrets','slurpit_netbox','netbox_branching']
+
+PLUGINS_CONFIG = {
+    'netbox_bgp': {},
+    'netbox_secrets': {},
+    'slurpit_netbox': {},
+    'netbox_branching': {}
+}
