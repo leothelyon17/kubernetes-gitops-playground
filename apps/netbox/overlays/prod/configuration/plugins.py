172177211189d0b5fb3be4fdfc76e24da139028f
Jeffrey Lyon <72824438+leothelyon17@users.noreply.github.com>
2025-07-19 18:33:03 -0400

    Update plugins.py

--- apps/netbox/overlays/prod/configuration/plugins.py
+++ apps/netbox/overlays/prod/configuration/plugins.py
@@ -1,8 +1,27 @@
-PLUGINS = ['netbox_bgp','netbox_secrets','slurpit_netbox','netbox_branching']
+PLUGINS = ['netbox_bgp','netbox_lifecycle','netbox_slm','netbox_secrets','slurpit_netbox','netbox_diode_plugin','netbox_branching']
 
 PLUGINS_CONFIG = {
     'netbox_bgp': {},
+    'netbox_lifecycle': {},
+    'netbox_slm': {},
     'netbox_secrets': {},
     'slurpit_netbox': {},
-    'netbox_branching': {}
+    'netbox_branching': {},
+    'netbox_diode_plugin': {
+        # # Auto-provision users for Diode plugin
+        # "auto_provision_users": False,
+
+        # Diode gRPC target for communication with Diode server
+        "diode_target": "grpc://diode-marathon-srvc.lib-nauto-apps.lab.aheadaviation.com/diode",
+
+        # # User allowed for Diode to NetBox communication
+        # "diode_to_netbox_username": "diode-to-netbox",
+
+        # # User allowed for NetBox to Diode communication
+        # "netbox_to_diode_username": "netbox-to-diode",
+
+        # User allowed for data ingestion
+        #"diode_username": "diode-ingestion",
+        "diode_username": "diode",
+    }
 }
